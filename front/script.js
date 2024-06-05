3a5ede9970bcf4bf239da802d6cb19e7c4747210
RAYAN IDRI <ridri@bouyguestelecom.fr>
2024-06-05 15:05:45 +0200

    Updated front & back - New feature In Progress

--- front/script.js
+++ front/script.js
@@ -22,6 +22,55 @@ socket.on('teamMembersUpdate', (teamMembers) => {
     document.getElementById('membersBlue').innerText = `Membres: ${teamMembers.blue}`;
 });
 
+socket.on('powerUp', (powerUp) => {
+    switch (powerUp.type) {
+        case 'star':
+            activateStarPowerUp();
+            break;
+        case 'bomb':
+            activateBombPowerUp();
+            break;
+        case 'jackpot':
+            activateJackpotPowerUp();
+            break;
+    }
+});
+
+function activateStarPowerUp() {
+    const originalIncrement = increment;
+    increment = function(team) {
+        originalIncrement(team);
+        originalIncrement(team);
+    };
+
+    setTimeout(() => {
+        increment = originalIncrement;
+    }, 10000);
+}
+
+function activateBombPowerUp() {
+    const originalIncrement = increment;
+    increment = function(team) {
+        scores[team] = Math.max(0, scores[team] - 5);
+        socket.emit('scoreUpdate', scores);
+    };
+
+    setTimeout(() => {
+        increment = originalIncrement;
+    }, 10000);
+}
+
+function activateJackpotPowerUp() {
+    increment = function(team) {
+        scores[team] += 500;
+        socket.emit('scoreUpdate', scores);
+    };
+
+    increment(currentTeam);
+
+    increment = originalIncrement;
+}
+
 document.getElementById('teamChoice').addEventListener('change', function() {
     const newTeam = this.checked ? 'red' : 'blue';
     document.getElementById('blue').style.pointerEvents = newTeam === 'blue' ? 'auto' : 'none';
